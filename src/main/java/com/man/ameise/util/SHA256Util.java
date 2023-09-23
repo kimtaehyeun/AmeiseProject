4d812154a341d3e88265918d035c77bc93377b5e
kimtaehyeun <rlaxogus2799@naver.com>
2023-09-23 23:26:18 +0900

    utilsetting

---
+++ src/main/java/com/man/ameise/util/SHA256Util.java
@@ -0,0 +1,9 @@
+package com.man.ameise.util;
+
+public class SHA256Util {
+
+	public static String getEncrypt(String source, String salt) {
+		return null;
+	}
+	
+}
---
+++ src/main/java/com/man/ameise/vo/BoardFileVO.java
@@ -0,0 +1,19 @@
+package com.man.ameise.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardFileVO {
+		
+	
+	private Long id;
+	
+	private String fileName;
+	private String oriName;
+	
+	
+	
+	
+}
