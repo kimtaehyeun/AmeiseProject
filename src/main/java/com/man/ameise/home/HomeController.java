056207b231fbc1361a08253719a5b910867cb9c6
kimtaehyeun <rlaxogus2799@naver.com>
2023-10-08 01:07:50 +0900

    homecontroller import 삭제

--- src/main/java/com/man/ameise/home/HomeController.java
+++ src/main/java/com/man/ameise/home/HomeController.java
@@ -1,12 +1,8 @@
 package com.man.ameise.home;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.man.ameise.service.MemberService;
-import com.man.ameise.vo.MemberVO;
 
 @Controller
 public class HomeController {
